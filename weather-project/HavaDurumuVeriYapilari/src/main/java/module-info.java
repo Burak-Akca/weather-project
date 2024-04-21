72e87826b273b0e4755823a513903183e4a88f7a
burak0756 <burakakca51@gmail.com>
2024-04-21 11:57:59 +0300

    optimizasyon dosyası silindi

--- weather-project/HavaDurumuVeriYapilari/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.havadurumuveriyapilari {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-
-
-    opens com.example.havadurumuveriyapilari to javafx.fxml;
-    exports com.example.havadurumuveriyapilari;
-}
